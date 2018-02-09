f47c5567e8abdb9b3a17849db45c75c03e61875e
王谦 <wangqian-0@thunisoft.com>
2018-02-09 10:57:09 +0800

    Initial commit

---
+++ src/com/thunisoft/demo/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.thunisoft.demo;
+
+/**
+ * @Author： wangqian
+ * @Description：${DESCRIPTION}
+ * @Date ：Created in  2018-02-09 10:35
+ */
+public class HelloWorld {
+    /**
+     * @Author：wangqian
+     * @Date：2018/2/9 10:43
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
